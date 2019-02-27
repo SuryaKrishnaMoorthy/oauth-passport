9de002cce7e20b7df268292052cb3312551a8382
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2019-02-27 00:12:56 -0800

    add passport setup

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add this to .git.ignore
+
+module.exports = {
+    google: {
+        clientID: "95808883560-tf2q736lfnikfmlkilke5vlg0fdv1523.apps.googleusercontent.com",
+        clientSecret: "Senur7lpoAYp6CTtI-29rkhW"
+    }
+}
