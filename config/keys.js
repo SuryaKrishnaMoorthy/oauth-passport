f07f2491dfc6045c8450a4b28f20ab96fc6e90fa
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2019-02-27 10:15:09 -0800

    update

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-// add this to .git.ignore
-
-module.exports = {
-    google: {
-        clientID: "95808883560-tf2q736lfnikfmlkilke5vlg0fdv1523.apps.googleusercontent.com",
-        clientSecret: "Senur7lpoAYp6CTtI-29rkhW"
-    }
-}
